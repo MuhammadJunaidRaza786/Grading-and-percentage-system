1461e7fc2dbc41eb9983b8446857ad9b16eecf39
Junaid Raza <88501739+MuhammadJunaidRaza786@users.noreply.github.com>
2024-03-06 15:22:05 +0500

    ....

---
+++ app.js
@@ -0,0 +1,23 @@
+var T_Marks = +prompt("Enter the total numbers");
+var M_Marks = +prompt("Enter the Maximum Marks")
+var percentage = T_Marks/M_Marks * 100
+document.write("Your percentage is " + percentage + "% <br>")
+
+if (percentage >= 90){
+    document.write("Your Grade is A+")
+}
+else if (percentage >= 80){
+    document.write("Your Grade is A")
+}
+else if (percentage >= 70) {
+    document.write("Your Grade is B")
+}
+else if (percentage >= 60) {
+    document.write("Your Grade is C")
+}
+else if (percentage >= 50) {
+    document.write("Your Grade is D")
+}
+else {
+    document.write("Sorry, You are Failed")
+}
